767cec0d342bf4e3fd8ac1050c06b34686c44fda
mariusss11 <mariuscarchilan07@gmail.com>
2025-08-31 12:32:47 +0300

    Move the car a litlle bit down

--- src/pages/cars/Cars.tsx
+++ src/pages/cars/Cars.tsx
@@ -62,7 +62,7 @@ export const Cars: React.FC = () => {
   });
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 p-20">
       {/* Results header */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <motion.div
