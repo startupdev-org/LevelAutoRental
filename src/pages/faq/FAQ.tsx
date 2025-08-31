95b967b0906ceb12ab509335324f278bb41a8eb3
mariusss11 <mariuscarchilan07@gmail.com>
2025-08-31 12:14:19 +0300

    Improve the faq page

--- src/pages/faq/FAQ.tsx
+++ src/pages/faq/FAQ.tsx
@@ -3,14 +3,12 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import { fadeInUp, staggerContainer } from '../../utils/animations';
 import { useInView } from '../../hooks/useInView';
-import { Hero } from "./Hero";
 import { Questions } from "./Questions";
 
 
 export const FAQ: React.FC = () => {
   return (
     <div>
-      {/* <Hero /> */}
       <Questions />
     </div>
   )
