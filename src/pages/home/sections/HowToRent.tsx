bc2a44a7e10b241b015edcca70cf3a1236120f31
Dowody <dowody2007@gmail.com>
2025-10-27 20:03:33 +0200

    HowToRent Bug Fix For Mobile V2

--- src/pages/home/sections/HowToRent.tsx
+++ src/pages/home/sections/HowToRent.tsx
@@ -111,7 +111,7 @@ export const HowToRent: React.FC = () => {
 
                             {/* Connecting Arrow */}
                             {index < steps.length - 1 && (
-                                <div className="hidden lg:block absolute top-1/2 -right-16 transform -translate-y-1/2 z-10">
+                                <div className="hidden lg:block absolute top-1/2 -right-10 transform -translate-y-1/2 z-10">
                                     <div className="w-6 h-6 bg-red-500 rounded-full flex items-center justify-center">
                                         <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
                                             <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
