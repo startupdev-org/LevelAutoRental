abe9ee9b31b06c4e622884194575e92dd1c78025
mariusss11 <mariuscarchilan07@gmail.com>
2025-11-03 19:16:07 +0200

    Make the dashboard page wider

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -342,7 +342,7 @@ export const Admin: React.FC = () => {
                 className="transition-all duration-300"
                 style={{ marginLeft: sidebarCollapsed ? 72 : 280, paddingTop: 32 }}
             >
-                <div className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-[1300px] mx-auto px-4 sm:px-6 lg:px-8">
                     {renderContent()}
                 </div>
             </main>
